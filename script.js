912894e2a5fcfa004929f853d082c049525f2b07
Hrexandro <marcin_nowak_hrx@o2.pl>
2021-06-05 20:53:48 +0200

    add field-filling

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*
+what should the player and game objects do?
+*/
 const gameBoard = (function(){
     let boardArray=[];
 
@@ -11,19 +14,28 @@ const gameBoard = (function(){
 
     for (i=0;i<9;i++){//add the elements
         let newField = document.createElement('div');
-        newField.setAttribute('id',`f-${i}`);//maybe introduce better naming to specify row and column?
+        newField.setAttribute('id',`${i}`);//maybe introduce better naming to specify row and column?
         newField.setAttribute('class','field')
         gameBoardElement.appendChild(newField)
 
-        newField.innerText="X"//to remove later when they are empty
+        //newField.innerText="X"//to remove later when they are empty
+        newField.addEventListener('click',fillField)
         
     }
 
-    function fillField (){
-
+    function fillField (event){
+        console.log(event);
+        console.log(game.currentPlayer);
+        console.log(game.currentPlayer);
+        if (event.target.innerText===""){
+            event.target.innerText = `${game.currentPlayer}`;
+            boardArray[event.target.getAttribute('id')] = game.currentPlayer;
+            (game.currentPlayer==="O")? game.currentPlayer="X" : game.currentPlayer="O";   
+        }
     }
     return {
         fillField,
+        boardArray,
     }
 
 })();
@@ -31,6 +43,12 @@ const gameBoard = (function(){
 const game = (function(){
     let currentPlayer="O";
 
+
+
+
+    return {
+        currentPlayer,
+    }
 
 })();
 
